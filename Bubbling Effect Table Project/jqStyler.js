206f152ab7a689ec9cdbb8e8085b42d0fa727c85
Devarzia Barkins <devarziabarkins@gmail.com>
2015-03-20 00:16:40 -0400

    Bootstrap-jQuery-JavaScript
    A compilation of simple websites using Bootstrap, JavaScript, and jQuery

---
+++ Bubbling Effect Table Project/jqStyler.js
@@ -0,0 +1,76 @@
+﻿$(function ()
+{
+    $(".select:checkbox").change(function ()
+        // Also can be typed as $("input.select[type=checkbox]")
+        // Also can be typed as $("input[type=checkbox]")
+    {
+        // Inside an Event Handler
+        // "this" pointer references the element
+        // on which the event fired
+
+        // event.target references the element
+        // on which the evvent occured
+
+        // When using bubbling, event.target != this
+        // otherwise they reference the same element
+
+        if (this.checked)
+        {
+            $(this).parents("tr").addClass("select");
+        }
+        else
+        {
+            $(this).parents("tr").removeClass("select");
+        }
+    });
+});
+
+// Alternative Method
+//$(function () {
+//    $("#Bubbling").change(function (event)
+//        // Also can be typed as $("input.select[type=checkbox]")
+//        // Also can be typed as $("input[type=checkbox]")
+//    {
+//        // Inside an Event Handler
+//        // "this" pointer references the element
+//        // on which the event fired
+
+//        // event.target references the element
+//        // on which the evvent occured
+
+//        // When using bubbling, event.target != this
+//        // otherwise they reference the same element
+
+//        if (event.target.checked) {
+//            $(event.target).parents("tr").addClass("select");
+//        }
+//        else {
+//            $(event.target).parents("tr").removeClass("select");
+//        }
+//    });
+//});
+
+// Alternative method with Bubbling
+$(function () {
+    $("#Bubbling").on("change", ":checkbox", function ()
+        // Also can be typed as $("input.select[type=checkbox]")
+        // Also can be typed as $("input[type=checkbox]")
+    {
+        // Inside an Event Handler
+        // "this" pointer references the element
+        // on which the event fired
+
+        // event.target references the element
+        // on which the evvent occured
+
+        // When using bubbling, event.target != this
+        // otherwise they reference the same element
+
+        if (this.checked) {
+            $(this).parents("tr").addClass("select");
+        }
+        else {
+            $(this).parents("tr").removeClass("select");
+        }
+    });
+});
